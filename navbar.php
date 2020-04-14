1a7afcd0c39ce1f89dfea9b40fa9931e7a49447b
Grognard-dev <lefevre.gregoire19@gmail.com>
2020-04-14 23:13:59 +0200

    navbar

---
+++ navbar.php
@@ -0,0 +1,50 @@
+<nav class="navbar navbar-expand-lg navbar-dark text-white " style="background-color:#002c4e">
+  <a class="navbar-brand" href="#">BSF</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse " id="navbarNavDropdown">
+    <ul class="nav justify-content-end ">
+      <li class="nav-item ">
+        <a class="nav-link text-white" href="#">s'inscrire</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="#">Connexion</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Article
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item d-flex" href="#"><img src="img/index.png" class="rounded mx-auto d-block w-25" alt="...">
+          <p>Sport</p>
+          </a>
+           <a class="dropdown-item d-flex " href="#"><img src="img/police.png" class="rounded mx-auto d-block w-25" alt="...">
+          <p>Police</p>
+          </a>
+           <a class="dropdown-item d-flex " href="#"><img src="img/femme.png" class="rounded mx-auto d-block w-25" alt="...">
+          <p>femme</p>
+          </a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Les Tops
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">le top de la loose</a>
+          <a class="dropdown-item" href="#">les avions de chasses</a>
+          <a class="dropdown-item" href="#"> le top 10 de loose hebdomadaire</a>
+        </div>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link text-white " href="#">Contact</a>
+      </li>
+    </ul>
+     <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
