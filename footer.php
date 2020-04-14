897ef7af6b59c90267a8fef51a76f6fd9227aadf
KadTas <tas@simplon-charleville.fr>
2020-04-14 16:57:27 +0200

    14/4/2020

---
+++ footer.php
@@ -0,0 +1,28 @@
+<link rel = "stylesheet" type = "text/css" href = "./css/footer.css" />
+<footer>
+<div class=trifooter>
+    <div class=trifooterpart>
+    <div class=zelogo>Logo</div>
+    <div class=listabout>
+    <ul>Lorem Ipsum</ul>
+    <ul>Lorem Ipsum</ul>
+    <ul>Lorem Ipsum</ul>
+    </div>
+    <div>
+    <p>2020 Simplon</p>
+    </div>
+    </div>
+    <div class=trifooterpart>
+    <p>Plan du site</p>
+    <ul>Lorem Ipsum</ul>
+    <ul>Lorem Ipsum</ul>
+    <ul>Lorem Ipsum</ul>
+    </div>
+    <div class=trifooterpart>
+    <p>A propos de BSF</p>
+    <ul>Lorem Ipsum</ul>
+    <ul>Lorem Ipsum</ul>
+    <ul>Lorem Ipsum</ul>
+    </div>
+</div>
+</footer>
